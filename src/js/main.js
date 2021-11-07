94450aefb722f99fcc7fb1de7d92f3f4fa2fdf9c
Mahinul Mostafa Abid <himahinulabid@gmail.com>
2021-11-07 11:04:20 +0600

    made header more responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 let headerController = (()=>{
     const windowViewHeight = window.innerHeight;
+    const windowViewWidth = window.innerWidth;
     console.log(windowViewHeight);
+    if(windowViewWidth < 500){
+        console.log("less than 600");
+        if(windowViewHeight < 400){
+            console.log("don't add the style");
+        }else if(windowViewHeight > 401){
+            let elementS = document.querySelector(".hero-section");
+            elementS.classList.remove("hero-section-first-style");
+            elementS.classList.add("hero-section-second-style");
+            document.querySelector("header").style.height ="100vh";
+        }
+    }
 })();
