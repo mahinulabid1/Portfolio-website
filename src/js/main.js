5c62958c9cf73d1f7f6872967d696b1f53a0638f
Mahinul Mostafa Abid <himahinulabid@gmail.com>
2021-11-09 19:40:56 +0600

    deleted downloaded fonts and optimized

--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,14 @@ let headerController = (()=>{
         console.log(windowViewHeight);
         if(windowViewWidth < 500){
             console.log("less than 600");
-            if(windowViewHeight < 400){
+            if(windowViewHeight < 600){
                 console.log("don't add the style");
-            }else if(windowViewHeight > 401){
+            }else if(windowViewHeight > 601){
                 elementS.classList.remove("hero-section-first-style");
                 elementS.classList.add("hero-section-second-style");
                 document.querySelector("header").style.height ="100vh";
                 document.querySelector(".Second-section").style.marginTop = "0";
+                document.querySelector(".scroll-down-i-container").style.display= "block";
             }
         }
         else if(windowViewWidth > 501){
@@ -20,6 +21,7 @@ let headerController = (()=>{
             elementS.classList.remove("hero-section-second-style");
             document.querySelector(".Second-section").style.marginTop = "262px";
             document.querySelector("header").style.height ="auto";
+            document.querySelector(".scroll-down-i-container").style.display= "none";
         }
     }
     loadingEvent();
@@ -27,5 +29,14 @@ let headerController = (()=>{
         console.log("resizing");
         loadingEvent();
     });
+
+
+    document.querySelector(".scroll-down-i-container").addEventListener("click", ()=>{
+        let k = document.querySelector(".Second-section");
+        k.scrollIntoView(true);
+        k.style.paddingTop="60px";
+        // window.scrollTo(0, 390);
+        console.log("scrll clicked");
+    }); 
     
 })();
