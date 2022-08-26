766f8fcf940488aa5ee7524790d2ba2f26f4095f
MahinulA <himahinulabid@gmail.com>
2022-08-27 00:07:39 +0600

    added a new button

--- src/js/profileOpen.js
+++ src/js/profileOpen.js
@@ -1,5 +1,6 @@
 let ProfileFunction= (()=>{
     let btnOne = document.querySelector(".m-b-pro");
+    let aboutmeTopbtn = document.querySelector(".aboutme-top-nav-btn");
     let closeBtn = document.getElementById("close-btn");
     let OpenFunction= ()=>{
         console.log("open fucntion executed");
@@ -9,6 +10,7 @@ let ProfileFunction= (()=>{
         console.log("open fucntion executed");
         document.querySelector(".full-profile-section").style.display= "none";
     }
+    aboutmeTopbtn.addEventListener("click", OpenFunction);
     btnOne.addEventListener("click", OpenFunction);
     closeBtn.addEventListener("click", CloseFunction);
 })();
